bc110b8df05392cb33a94803f52435ce2289dbc1
Rilindo Foster <rilindo.foster@monzell.com>
2015-02-05 22:31:16 -0600

    Updated berks file

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,1 +1,8 @@
-# to do
+require 'chefspec'
+
+describe 'ddclient::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+  it 'installs a package with the default action' do
+    expect(chef_run).to install_package('default_action')
+  end
+end
