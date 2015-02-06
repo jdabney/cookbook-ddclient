0649fd68216fb24850b9e7775ddb37139edb19b9
Rilindo Foster <rilindo.foster@monzell.com>
2015-02-05 23:39:01 -0600

    Updated foodcritic, metadata

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "rilindo.foster@monzell.com"
 license          "Apache 2.0"
 description      "Installs ddclient"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          "1.1.2"
 name             "ddclient"
